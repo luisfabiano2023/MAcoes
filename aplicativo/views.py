db745049c54a2fea27c34cb5588cd71bf1c31ccb
luisfabiano2023 <luisfabiano2022.1@gmail.com>
2025-07-26 06:14:31 -0300

    resolvendo um bug de config

--- aplicativo/views.py
+++ aplicativo/views.py
@@ -10,6 +10,7 @@ from django.shortcuts import get_object_or_404
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
+from rest_framework.authtoken.models import Token
 
 @api_view(['POST'])
 def signup(request):
